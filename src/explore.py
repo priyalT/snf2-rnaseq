5520b7e33e0fc1d7aa4e45dd4bcb0ed6a72de60f
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-09-08 12:15:26 +0530

    exploration of FastQC

---
+++ src/explore.py
@@ -0,0 +1,19 @@
+import gzip
+import numpy as np
+from Bio import SeqIO
+import matplotlib.pyplot as plt
+
+n = 100_000
+per_read = []   
+grid     = []  
+
+with gzip.open("data/ERR458495.fastq.gz", "rt") as handle:
+    for i, record in enumerate(SeqIO.parse(handle, "fastq")):
+        if i >= n:
+            break
+        q = record.letter_annotations["phred_quality"]
+        per_read.append(sum(q) / len(q))
+        grid.append(q)
+
+grid = np.array(grid)              
+per_position = grid.mean(axis=0)    
