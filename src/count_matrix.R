0575c5f74198defaa77790df8cf03398792ca164
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-09-16 15:52:07 +0530

    create count matrix

--- src/count_matrix.R
+++ src/count_matrix.R
@@ -1,12 +1,56 @@
 library(readr)
+library(dplyr)
+library(glue)
 
-#step 1: try to make 1 count matrix by reading one file: genes x column 2 only
-#step 2: figure out how to make multiple dataframes
-#step 3: join all the dataframes to make a
-#        genes X samples dataframe, samples = 12
+coldata <- read.csv("../data/samplesheet.csv")
+samples <- coldata$sample
 
-counts  <- as.data.frame(
-            read_delim("results/alignment/star/ERR458502_ReadsPerGene.out.tab",
-                       delim = "\t", col_names = TRUE))
-counts <- as.data.frame(counts)
-typeof(counts)
+for (sample in samples) {
+  counts  <- as.data.frame(
+    read_delim(glue("../results/alignment/star/{sample}_ReadsPerGene.out.tab"),
+               delim = "\t", col_names = FALSE))
+  counts_df <- as.data.frame(counts[, -1])
+  rownames(counts_df) <- counts$X1
+  counts_df_final <- counts_df %>%
+    select(1)
+  colnames(counts_df_final)[colnames(counts_df_final) == 'X2'] <- glue("{sample}")
+  assign(paste0("counts_", sample), counts_df_final)
+}
+
+merged_df <- merge(counts_ERR458495, counts_ERR458502, counts_ERR458509, counts_ERR458517,
+                   counts_ERR458528, counts_ERR458535, counts_ERR458552, counts_ERR458882,
+                   counts_ERR458887, counts_ERR458905, counts_ERR458906, counts_ERR458921, 
+                   by="row.names")
+rownames(merged_df) <- merged_df$Row.names
+merged_df$Row.names <- NULL
+
+merged_df <- Reduce(
+  function(x, y) {
+    
+    z <- merge(x, y, by = "row.names", all = TRUE)
+    
+    rownames(z) <- z$Row.names
+    z$Row.names <- NULL
+    
+    return(z)
+  },
+  list(
+    counts_ERR458495,
+    counts_ERR458502,
+    counts_ERR458509,
+    counts_ERR458517,
+    counts_ERR458528,
+    counts_ERR458535,
+    counts_ERR458552,
+    counts_ERR458882,
+    counts_ERR458887,
+    counts_ERR458905,
+    counts_ERR458906,
+    counts_ERR458921
+  )
+)
+
+
+count_matrix <- as.matrix(merged_df)
+count_matrix <- count_matrix[, rownames(coldata)]
+
