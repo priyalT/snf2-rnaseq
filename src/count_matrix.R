d8d60d79fee8fdf4b26585e1bdd5256693e79088
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-09-17 11:40:16 +0530

    create count matrix

--- src/count_matrix.R
+++ src/count_matrix.R
@@ -1,6 +1,8 @@
 library(readr)
 library(dplyr)
 library(glue)
+library(DESeq2)
+library(ggplot2)
 
 coldata <- read.csv("../data/samplesheet.csv")
 samples <- coldata$sample
@@ -16,13 +18,6 @@ for (sample in samples) {
   colnames(counts_df_final)[colnames(counts_df_final) == 'X2'] <- glue("{sample}")
   assign(paste0("counts_", sample), counts_df_final)
 }
-
-merged_df <- merge(counts_ERR458495, counts_ERR458502, counts_ERR458509, counts_ERR458517,
-                   counts_ERR458528, counts_ERR458535, counts_ERR458552, counts_ERR458882,
-                   counts_ERR458887, counts_ERR458905, counts_ERR458906, counts_ERR458921, 
-                   by="row.names")
-rownames(merged_df) <- merged_df$Row.names
-merged_df$Row.names <- NULL
 
 merged_df <- Reduce(
   function(x, y) {
@@ -50,19 +45,6 @@ merged_df <- Reduce(
   )
 )
 
-
-count_matrix <- as.matrix(merged_df)
-
-row_sub = apply(count_matrix, 1, function(row) all(row ==0 ))
-row_N <- !rownames(count_matrix) %in% c(
-  "N_ambiguous",
-  "N_multimapping",
-  "N_noFeature",
-  "N_unmapped"
-)
-
-count_matrix <- count_matrix[row_N, ]
-count_zero_rows = count_matrix[row_sub,]
-
-min(count_matrix)
-max(count_matrix)
+class(merged_df)
+write.csv(merged_df, 
+          "../results/countmatrix.csv")
