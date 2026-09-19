5e0f70826b35142b7b6d730abcc8c24f256d90f0
Priyal Tripathi <priyaltripathi2910@gmail.com>
2026-09-19 21:29:06 +0530

    debug

--- src/alignment.sh
+++ src/alignment.sh
@@ -3,6 +3,12 @@
 
 tail -n +2 ../data/samplesheet.csv | cut -d, -f1 | while read -r s; do
     echo "=== $s ==="
+
+    if [ ! -s ../data/raw/${s}_trimmed.fq ]; then
+        echo "SKIP $s - no trimmed file" >&2
+        continue
+    fi
+
     rm -rf ../results/alignment/star/${s}_*
 
     STAR --genomeDir ../scerevisiae_star/ \
@@ -12,8 +18,7 @@ tail -n +2 ../data/samplesheet.csv | cut -d, -f1 | while read -r s; do
          --outSAMtype None \
          --runThreadN 4
 
-    if [ ! -s ../data/raw/${s}_trimmed.fq ]; then
-        echo "SKIP $s - no trimmed file" >&2
-        continue
+    if [ ! -s ../results/alignment/star/${s}_ReadsPerGene.out.tab ]; then
+        echo "FAILED: $s" >&2
     fi
 done
--- src/index_creation_star.sh
+++ src/index_creation_star.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
- star --runMode genomeGenerate \
- --runThreadN 4 \
- --genomeDir scerevisiae_star \
- --genomeFastaFiles genome/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa \
- --sjdbGTFfile genome/Saccharomyces_cerevisiae.R64-1-1.114.gtf \
- --sjdbOverhang 50 \
- --genomeSAindexNbases 11
+# Run from src/
+
+mkdir -p ../scerevisiae_star
+
+STAR --runMode genomeGenerate \
+     --runThreadN 4 \
+     --genomeDir ../scerevisiae_star \
+     --genomeFastaFiles ../genome/Saccharomyces_cerevisiae.R64-1-1.dna.toplevel.fa \
+     --sjdbGTFfile ../genome/Saccharomyces_cerevisiae.R64-1-1.114.gtf \
+     --sjdbOverhang 50 \
+     --genomeSAindexNbases 11
